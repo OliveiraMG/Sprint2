7786af4a8ced911a1d7dd4574adc8b8562687bc6
cesarbmartins <cesar.martins@sptech.school>
2022-10-17 15:21:00 -0300

    Banco de dados pronto

--- Banco de Dados/script-bd-thermodata.sql
+++ Banco de Dados/script-bd-thermodata.sql
@@ -9,23 +9,12 @@ logradouro varchar(45),
 bairro varchar(45),
 cep char(8),
 cidade varchar(45),
-estado varchar(45)
+estado varchar(45),
+email varchar(45),
+tel char(11),
+senha varchar(24)
 );
 
-create table usuario (
-idUsuario int,
-fkEmpresa int,
-constraint fkEmp foreign key(fkEmpresa) references empresa(idEmpresa),
-primary key (idUsuario,fkEmpresa),
-nomeUsuario varchar(45),
-sobrenome varchar(45),
-email varchar(50),
-tel char(11),
-username varchar(45),
-senha varchar(24),
-fkRepresentante int,
-constraint fkRep foreign key(fkRepresentante) references usuario(idUsuario)
-);
 
 create table dataCenter(
 idDatacenter int,
@@ -55,24 +44,14 @@ fkSensor int,
 foreign key (fkSensor) references sensor(idSensor),
 primary key (idMetrica,fkSensor)
 );
-alter table usuario add constraint chkEmail check (email LIKE '%@%.%');
-alter table usuario add constraint chkSenha check (senha like '%!%' or senha like'%.%' or senha like '%@%' or senha like '%$%' or senha like '%*%' or senha like '%&%');
+alter table empresa add constraint chkEmail check (email LIKE '%@%.%');
+alter table empresa add constraint chkSenha check (senha like '%!%' or senha like'%.%' or senha like '%@%' or senha like '%$%' or senha like '%*%' or senha like '%&%');
  
  
 insert into empresa values 
- (null,'Sptech','10921092313',null,'paulista','09876543','são paulo','são paulo'),
- (null,'ibm','10211219324','avenida','vila mariana','09812910', 'são paulo','são paulo');
+ (null,'Sptech','10921092313',null,'paulista','09876543','são paulo','são paulo','sptech@sptech.school','11956789012','sptech123.'),
+ (null,'ibm','10211219324','avenida','vila mariana','09812910', 'são paulo','são paulo','img@outlook.com','11978654433','12345!');
  
-insert into usuario values
- (1,1,'Samuel','Bryan','samuelbryan@gmail.com','11987543200','SamuelB','senha123!.',null);
-
-insert into usuario values 
-(2,1,'Marcus','Vinicius','marcusvinicius@gmail.com','11913973613','MarcusV','senha654!.',1),
-(3,1,'Giovanana','Miniguiti','giovannaminiguiti@gmail.com','11965431122','GioMin','senha4321!!',1);
-insert into usuario values
-(1,2,'Igor','Gomes','igorg@gmail.com','11912347651','IgorG','Senha12!',null),
-(2,2,'Roberto','Silva', 'rosilva@outlook.com','11983457231','rSilva','senha45!',1),
-(3,2,'Marta','Silva', 'marta@outlook.com','11983259573','MartaSilva','Password1!',1);
 
 
 insert into dataCenter values
@@ -98,13 +77,10 @@ insert into metrica values
 (null,28.7,49,'2022-10-08 15:44:18',4);
 
 select * from empresa;
-select * from usuario;
 select * from dataCenter;
 select * from sensor;
 select * from metrica;
 
-select * from empresa join usuario 
-on fkEmpresa= idEmpresa;
 
 select * from empresa join dataCenter
 on fkEmpresa=idEmpresa;
