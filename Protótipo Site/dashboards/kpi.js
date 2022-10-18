81b72fbf933d16533e2f0280f672738bf19b2b9c
gvmgonsptech <marcus.goncalves@sptech.school>
2022-10-18 19:33:27 -0300

    Temperatura e Umidade mudam de 2 em 2 segundos

--- Protótipo Site/dashboards/kpi.js
+++ Protótipo Site/dashboards/kpi.js
@@ -7,31 +7,87 @@ function mudarkpi() {
     //"animação" mostra e muda as metricas de temperatura e umidade, setinterval- intervalo de tempo, setimeout - executa depois de um certo tempo
     // 2000 são 2 segundos
 
-    setInterval(() => {
-        setTimeout(() => {
-            tempreal.innerHTML = `22.6°C`
-        }, 2000);
-        setTimeout(() => {
-            tempreal.innerHTML = `22.8°C`
-        }, 4000);
-        setTimeout(() => {
-            tempreal.innerHTML = `23.2°C`     //"animação" que muda o valor de temperatura "em tempo real" 
-        }, 6000);
-        setTimeout(() => {
-            tempreal.innerHTML = `23.1°C`
-        }, 8000);
-        setTimeout(() => {
-            tempreal.innerHTML = `22.7°C`
-        }, 10000);
-        setTimeout(() => {
-            tempreal.innerHTML = `22.6°C`
-        }, 5000);
-    }, 8000);
+   setInterval(() => {
+            setTimeout(() => {
+                tTempreal.innerHTML = `Sensor 2`
+                tempreal.innerHTML = `17.1°C`
+            }, 2000);
+            setTimeout(() => {
+                tTempreal.innerHTML = `Sensor 3`
+                tempreal.innerHTML = `20.6ºC`     //"animação" que muda o valor de temperatura "em tempo real" 
+            }, 4000);
+            setTimeout(() => {
+                tTempreal.innerHTML = `Sensor 4`
+                tempreal.innerHTML = `19.2°C`
+            }, 6000);
+            setTimeout(() => {
+                tTempreal.innerHTML = `Sensor 5`
+                tempreal.innerHTML = `22.7°C`
+            }, 8000);
+            setTimeout(() => {
+                tTempreal.innerHTML = `Sensor 6`
+                tempreal.innerHTML = `22.1°C`
+            }, 10000);
+            setTimeout(() => {
+                tTempreal.innerHTML = `Sensor 7`
+                tempreal.innerHTML = `19.3°C`
+            }, 12000);
+            setTimeout(() => {
+                tTempreal.innerHTML = `Sensor 8`
+                tempreal.innerHTML = `17.9°C`
+            }, 14000);
+            setTimeout(() => {
+                tTempreal.innerHTML = `Sensor 9`
+                tempreal.innerHTML = `19.8°C`
+            }, 16000);
+            setTimeout(() => {
+                tTempreal.innerHTML = `Sensor 1`
+                tempreal.innerHTML = `18.6°C`
+            }, 18000);
+    }, 20000);
+
+       setInterval(() => {
+            setTimeout(() => {
+                tUmidreal.innerHTML = `Sensor 2`
+                umidReal.innerHTML = `48.5%`
+            }, 2000);
+            setTimeout(() => {
+                tUmidreal.innerHTML = `Sensor 3`
+                umidReal.innerHTML = `48.3%`     //"animação" que muda o valor de temperatura "em tempo real" 
+            }, 4000);
+            setTimeout(() => {
+                tUmidreal.innerHTML = `Sensor 4`
+                umidReal.innerHTML = `47.2%`
+            }, 6000);
+            setTimeout(() => {
+                tUmidreal.innerHTML = `Sensor 5`
+                umidReal.innerHTML = `46.8%`
+            }, 8000);
+            setTimeout(() => {
+                tUmidreal.innerHTML = `Sensor 6`
+                umidReal.innerHTML = `48.2%`
+            }, 10000);
+            setTimeout(() => {
+                tUmidreal.innerHTML = `Sensor 7`
+                umidReal.innerHTML = `49.1%`
+            }, 12000);
+            setTimeout(() => {
+                tUmidreal.innerHTML = `Sensor 8`
+                umidReal.innerHTML = `49.4%`
+            }, 14000);
+            setTimeout(() => {
+                tUmidreal.innerHTML = `Sensor 9`
+                umidReal.innerHTML = `49.8%`
+            }, 16000);
+            setTimeout(() => {
+                tUmidreal.innerHTML = `Sensor 1`
+                umidReal.innerHTML = `47.2%`
+            }, 18000);
+    }, 20000);
     //---------------------------------------------------------------
 
 
     //"animação" mostra e muda as metricas de temperatura e umidade, setinterval- intervalo de tempo, setimeout - executa depois de um certo tempo
-
     setInterval(() => {
 
         setTimeout(() => {
@@ -54,8 +110,6 @@ function mudarkpi() {
         }, 9000);
 
     }, 18000);
-
-
 
 }
 
