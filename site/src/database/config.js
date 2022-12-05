586e02c97b56a71c7626ee0f109bcf75e47a4d4b
SamuelOliveiraSB <samuel.oliveira@sptech.school>
2022-12-05 18:55:01 -0300

    alteração nas cores dos titulos dos graficos

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "Thermodata",
     user: "root",
-    password: "Marcus27@@",
+    password: "12345678",
 };
 
 function executar(instrucao) {
