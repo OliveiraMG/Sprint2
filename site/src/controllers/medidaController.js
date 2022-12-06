c0cc7c864bd7ff079c67fa6bb27de5d1cae3fb5a
miniguitigi <giovanna.franca@sptech.school>
2022-12-06 15:22:36 -0300

    Requisição de dados dashboard principal ok, faltando somento plotar os dados

--- site/src/controllers/medidaController.js
+++ site/src/controllers/medidaController.js
@@ -2,7 +2,7 @@ var medidaModel = require("../models/medidaModel");
 
 function buscarUltimasMedidas(req, res) {
 
-    const limite_linhas = 7;
+    const limite_linhas = 15;
 
     var tipo = req.params.tipo
     
@@ -43,11 +43,31 @@ function buscarMedidasEmTempoReal(req, res) {
     });
 }
 
+function metricas(req, res) {
+
+
+    console.log(`Recuperando medidas em tempo real`);
+
+    medidaModel.buscarMedidasEmTempoReal().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+
 
 
 
 module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
+    buscarMedidasEmTempoReal,
+    metricas
 
 }
